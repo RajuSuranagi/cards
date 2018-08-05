a8b0c88b0f38f50736274aa7678dbabcb5ac8eee
Raju Gangadhar Suranagi <rsuranagi@tekion.com>
2018-08-06 00:49:40 +0530

    Ability to print and deal the cards

--- main.go
+++ main.go
@@ -1,15 +1,9 @@
 package main
 
-import "fmt"
-
 func main() {
-	cards := []string{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Ace of Clubs") // returns a new one
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
-}
-
-func newCard() string {
-	return "Ace of Spades!"
+	cards := newDeck()
+	cards.printCards()
+	hand, remainingCards := deal(cards, 5)
+	hand.printCards()
+	remainingCards.printCards()
 }
